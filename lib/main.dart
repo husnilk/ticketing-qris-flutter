a9fa0d925ce5a9367b4bd05d051cc32f9ac90f69
husnil.k <husnil.k@gmail.com>
2024-05-22 13:54:25 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/presentation/auth/splash_page.dart';
+
+import 'core/core.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Intensive Club Batch 17',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+        dialogTheme: const DialogTheme(elevation: 0),
+        textTheme: GoogleFonts.outfitTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        appBarTheme: AppBarTheme(
+          color: AppColors.white,
+          elevation: 0,
+          titleTextStyle: GoogleFonts.outfit(
+            color: AppColors.primary,
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+          iconTheme: const IconThemeData(
+            color: AppColors.black,
+          ),
+          centerTitle: true,
+        ),
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/presentation/auth/login_dart.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/presentation/home/main_page.dart';
+
+import '../../core/core.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+        final emailController = TextEditingController();
+    final passwordController = TextEditingController();
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Stack(
+        children: [
+          SizedBox(
+            height: 260.0,
+            child: Center(
+              child: Assets.images.logoWhite.image(height: 55.0),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SingleChildScrollView(
+              child: ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(20.0)),
+                child: ColoredBox(
+                  color: AppColors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 28.0, vertical: 44.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomTextField(
+                          controller: emailController,
+                          label: 'Email',
+                          isOutlineBorder: false,
+                        ),
+                        const SpaceHeight(36.0),
+                        CustomTextField(
+                          controller: passwordController,
+                          label: 'Password',
+                          isOutlineBorder: false,
+                          obscureText: true,
+                        ),
+                        const SpaceHeight(86.0),
+                        Button.filled(
+                          onPressed: () {
+                            context.pushReplacement(const MainPage());
+                          },
+                          label: 'Login',
+                        ),
+                        const SpaceHeight(128.0),
+                        Center(
+                          child: Assets.images.logoCwb.image(height: 40.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
